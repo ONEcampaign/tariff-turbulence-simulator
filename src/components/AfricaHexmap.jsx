25e3862a9b95ae84ca487b829b9c160fcc4d0f7c
Fernando Becerra <becerrafernando@gmail.com>
2025-06-09 20:00:18 -0300

    add hexmap

---
+++ src/components/AfricaHexmap.jsx
@@ -0,0 +1,37 @@
+import * as d3 from 'npm:d3';
+import * as React from "npm:react";
+
+
+export function AfricaHexmap({
+    width, height, data, clickedCountry, onClick
+} = {}) {
+    const [hoveredCountry, setHoveredCountry] = React.useState('NONE');
+
+    const projection = d3.geoIdentity().reflectY(true).fitSize([width, height], data);
+    const path = d3.geoPath().projection(projection);
+    const opacity = 0.4;
+    
+    return (
+        <svg width={width} height={height}>
+            {data.features.map(feature => {
+                const thisCountryIsClicked = clickedCountry === feature.properties.iso3;
+                return (
+                    <path
+                        fill={"lightgray"}
+                        stroke={"white"}
+                        stroke-width={"2px"}
+                        opacity={clickedCountry === 'All' ? 1 : (thisCountryIsClicked ? 1 : opacity)}
+                        d={path(feature)}
+                        onClick={() => {
+                            if (thisCountryIsClicked) {
+                                onClick('All');
+                            } else {
+                                onClick(feature.properties.iso3);
+                            }
+                        }}
+                    />
+                )
+            })}
+        </svg>
+    )
+}
--- src/index.md
+++ src/index.md
@@ -4,14 +4,24 @@ import { Deck } from './components/Deck.js';
 import { Legend } from './components/Legend.js';
 import { ChartTitle } from './components/ChartTitle.js';
 import { ChartSubTitle } from './components/ChartSubTitle.js';
+import { AfricaHexmap } from './components/AfricaHexmap.js';
 
 const data = FileAttachment("./data/us_africa_trade.csv").csv({typed: true});
+const geoData = FileAttachment("./data/africa_hexmap.geojson").json({typed: true});
+console.log(geoData)
+
+// TODO: merge geoData and data
 ```
 ```jsx
 
 function App() {
     const headline = "The Tariffs Game";
     const deck = "What’s the impact of tariffs on African economies? Use this simulation tool to find out.";
+
+    const [clickedCountry, setClickedCountry] = React.useState('All');
+
+    const width = 600;
+    const height = 600;
 
     return (
         <div className="wrapper">
@@ -20,6 +30,13 @@ function App() {
             <ChartTitle content={""} />
             <ChartSubTitle content={""} />
             <Legend />
+            <AfricaHexmap
+                width={width}
+                height={height}
+                data={geoData}
+                clickedCountry={clickedCountry}
+                onClick={setClickedCountry}
+            />
         </div>
     )
 }
