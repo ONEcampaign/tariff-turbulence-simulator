62ea6aab2a8226ecf8e1c6a7e87f84499ba09f93
mharoruiz <88669820+mharoruiz@users.noreply.github.com>
2025-06-11 15:59:53 +0200

    Update ExposureCard layout

--- src/components/ExposureCard.jsx
+++ src/components/ExposureCard.jsx
@@ -1,9 +1,20 @@
 export function ExposureCard({countryData} = {}) {
     return (
         <div className="tariff-card exposure-card">
-            <span>{countryData.country}</span><br></br>
-            <span>{countryData.etr}</span><br></br>
-            <span>{countryData.exports}</span>
+            <div className="left-column">
+                <span
+                    className="description-text">Efective Tariff Rate for {countryData.country} exports of {countryData.product}
+                </span>
+
+                <span className="exports-text">
+                    Exports to the US: <b>{countryData.exports}</b>
+                </span>
+            </div>
+            <div className="right-column">
+                <span className="tariff-text">
+                    {countryData.etr}
+                </span>
+            </div>
         </div>
     )
 };
