cf2c7448823a6638ea6bc93c6ca8ab24330d7cfc
mharoruiz <88669820+mharoruiz@users.noreply.github.com>
2025-06-13 15:35:10 +0200

    Import flag-icons

--- observablehq.config.js
+++ observablehq.config.js
@@ -7,7 +7,11 @@ const projectTitle = "Tariff Simulator"
 export default {
 
   title: projectTitle,
-  head: `<link rel="icon" href=${icon} type="image/png" sizes="32x32">`,
+  head: `<link rel="icon" href=${icon} type="image/png" sizes="32x32">
+         <link
+           rel="stylesheet"
+           href="https://cdn.jsdelivr.net/gh/lipis/flag-icons@7.3.2/css/flag-icons.min.css"
+         />`,
 
   header: generateHeader({title: projectTitle}),
   footer: generateFooter(),
