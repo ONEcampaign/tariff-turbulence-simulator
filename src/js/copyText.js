7612fd36a77039b75779392ab0675e8b033e3d2a
mharoruiz <88669820+mharoruiz@users.noreply.github.com>
2025-06-17 13:39:24 +0200

    Add subsection title and text to copyText.js

--- src/js/copyText.js
+++ src/js/copyText.js
@@ -3,3 +3,5 @@ export const deck = "What’s the impact of US tariffs on African economies? Use
 export const introText = "Tariffs are like taxes on exports, and some products get hit harder than others. The Effective Tariff Rate (ETR) reflects the average tariff a country faces, weighted by the value of its exports. It offers a clearer view of how US tariffs affect each economy.";
 export const legendTitle = "Exposure to US tariffs by country";
 export const legendSubtitle = "Based on the Effective Tarriff Rate (ETR)";
+export const subsectionTitle = "What that means for African trade";
+export const subsectionText = "A little description about what's going on here.";
