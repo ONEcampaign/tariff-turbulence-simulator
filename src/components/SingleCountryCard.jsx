5e74c4e9ec7f896eae43fc639fcbf73682ac5f5e
mharoruiz <88669820+mharoruiz@users.noreply.github.com>
2025-06-17 15:27:44 +0200

    Add singleCountryCard component

---
+++ src/components/SingleCountryCard.jsx
@@ -0,0 +1,72 @@
+import * as React from "npm:react";
+import * as d3 from "npm:d3";
+import { BarChart } from "../components/BarChart.js";
+import { formatPercentage, formatCurrency } from "../js/format.js";
+import { colorPalette } from "../js/colorPalette.js";
+import { riskThresholds } from "../js/riskThresholds.js";
+
+const colorScale = d3.scaleThreshold(
+    riskThresholds,
+    [colorPalette.low, colorPalette.medium, colorPalette.high]
+);
+
+export function SingleCountryCard({ data }) {
+    const allProductsData = data.find((d) => d.product === "All products");
+
+    const topProducts = data
+        .filter((d) => d.product !== "All products" && Number.isFinite(d.impact_usd))
+        .sort((a, b) => b.impact_usd - a.impact_usd)
+        .slice(0, 5);
+
+    return (
+        <div className="single-country-card">
+            <div className="single-country-card-swatch-plus-icon">
+                <div
+                    className="single-country-card-swatch"
+                    style={{
+                        backgroundColor:
+                            allProductsData.etr != null
+                                ? colorScale(allProductsData.etr)
+                                : colorPalette.na,
+                    }}
+                >
+          <span
+              className="single-country-card-swatch-text"
+              style={{
+                  color: allProductsData.etr != null ? "white" : "black",
+              }}
+          >
+            {allProductsData.etr != null
+                ? `ETR: ${allProductsData.etr}%`
+                : "No data"}
+          </span>
+                </div>
+                <span
+                    className={`fi fi-${allProductsData.iso2.toLowerCase()} fis`}
+                ></span>
+            </div>
+
+            <div className="single-country-card-header">
+                <h3 className="single-country-card-country-name">
+                    {allProductsData.country}
+                </h3>
+            </div>
+
+            <div className="single-country-card-row">
+                <h4 className="single-country-card-var-name">Total exposure</h4>
+                <p className="single-country-card-var-value">
+                    {formatCurrency(allProductsData.impact_usd)}
+                </p>
+            </div>
+
+            <div className="single-country-card-row">
+                <h4 className="single-country-card-var-name">% of GDP</h4>
+                <p className="single-country-card-var-value">
+                    {formatPercentage(allProductsData.impact_pct)}
+                </p>
+            </div>
+
+            <BarChart data={topProducts} />
+        </div>
+    );
+}
